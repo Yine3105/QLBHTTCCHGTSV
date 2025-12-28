27e02f04521613454a9c0e00dca3a0a78a03c89b
Dolly <nhungocnguyen871@gmail.com>
2025-12-28 16:40:20 +0700

    Add files via upload

---
+++ TrangChuController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cửa_hàng_sinh_viên_guitar.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        // GET: TrangChu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TrangChu/Create
+        [HttpPost]
+        public ActionResult Create(string TenKH, string SoDT, string Email, string DiaChi, string GhiChu, string CartJson)
+        {
+            // Test nhanh
+            return Content("Đặt hàng thành công: " + TenKH);
+        }
+
+    }
+}
